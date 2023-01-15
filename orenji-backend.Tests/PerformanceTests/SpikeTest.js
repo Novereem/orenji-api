2fdebf999c4e8a6e44c66916c4cd49a386ceb8d6
Overeem,Noah N.V <n.overeem@student.fontys.nl>
2023-01-15 23:44:59 +0100

    added test endpoint

--- orenji-backend.Tests/PerformanceTests/SpikeTest.js
+++ orenji-backend.Tests/PerformanceTests/SpikeTest.js
@@ -14,10 +14,10 @@ export const options = {
     },
 };
 
-const base_url = 'http://localhost:6000'
+const base_url = 'https://localhost:44353'
 export default () => {
     const responses = http.batch([
-        ['GET', `${base_url}/products/filter/filterProducts?Audience=Male`]
+        ['GET', `${base_url}/Test`]
     ])
     check(responses[0], {
         'status is' : res => res.status === 200
---
+++ orenji-backend/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace orenji_backend.Controllers
+{
+    [EnableCors("AllowCors")]
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public string PerformanceTest()
+        {
+            return "TEST";
+        }
+    }
+}
