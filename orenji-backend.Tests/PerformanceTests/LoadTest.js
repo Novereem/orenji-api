7b39bb286dde4fd5408584c8a0a3747a48fa9a8b
Overeem,Noah N.V <n.overeem@student.fontys.nl>
2023-01-15 23:56:34 +0100

    ipv6 pref loadtest

--- orenji-backend.Tests/PerformanceTests/LoadTest.js
+++ orenji-backend.Tests/PerformanceTests/LoadTest.js
@@ -2,6 +2,9 @@
 import {sleep, check} from 'k6';
 
 export const options = {
+    dns: {
+        policy: "preferIPv6"
+    },
     stages: [
         { duration: '30s', target: 150 },
         { duration: '1s', target: 150 },
@@ -11,6 +14,7 @@ export const options = {
         http_req_duration: ['p(99)<200'],
         http_req_failed: ['rate<0.01'],
     },
+    
 };
 
 const base_url = 'https://localhost:44353'
